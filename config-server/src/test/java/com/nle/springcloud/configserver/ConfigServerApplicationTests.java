aba25d77d6f3fb1c02c9e98616f0dea219ff1dad
nhungle <misssg85@gmail.com>
2022-10-09 07:31:06 -0500

    Init spring cloud config server

---
+++ config-server/src/test/java/com/nle/springcloud/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nle.springcloud.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
